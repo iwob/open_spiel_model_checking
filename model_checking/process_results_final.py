0d4e2a2886598cc8c4806f5132b9c2f2cf9ee7bf
Iwo Bladek <iwo.bladek@gmail.com>
2026-01-16 23:52:45 +0100

    Result processing

--- model_checking/process_results_final.py
+++ model_checking/process_results_final.py
@@ -33,15 +33,40 @@ def load_properties_from_dir(dir_path):
         res.append(load_properties(os.path.join(dir_path, file)))
     return res
 
+def map_max_depth_to_depth_percent(n, x):
+    d = {
+            4: {4: 0.25, 8: 0.5, 12: 0.75, 16: 1.0},
+            5: {6: 0.25, 13: 0.5, 19: 0.75, 25: 1.0},
+            6: {9: 0.25, 18: 0.5, 27: 0.75, 36: 1.0},
+            7: {12: 0.25, 25: 0.5, 37: 0.75, 49: 1.0},
+            8: {16: 0.25, 32: 0.5, 48: 0.75, 64: 1.0},
+            9: {20: 0.25, 41: 0.5, 61: 0.75, 81: 1.0},
+            10: {25: 0.25, 50: 0.5, 75: 0.75, 100: 1.0},
+         }
+    if n not in d:
+        return None
+    else:
+        d2 = d[n]
+        if x not in d2:
+            return None
+        else:
+            return d2[x]
+
 def process_dict(d):
     return {
             'm,n,k': d['m,n,k'],
-            # 'max_game_depth': d['max_game_depth'],
-            'avg.time_total': d['avg.time_total'],
-            # 'sum.result_0': d['sum.result_0'],
-            'sum.result_1': d['sum.result_1'],
-            # 'stddev.time_total': d['stddev.time_total'],
-            'avg.num_submodels': d['avg.num_submodels'],
+            'n': int(d['m,n,k'].split(',')[0][1:]),
+            'k': int(d['m,n,k'].split(',')[2][:-1].strip()),
+            'max_game_depth': int(d['max_game_depth']),
+            'depth_percent': map_max_depth_to_depth_percent(int(d['m,n,k'].split(',')[0][1:]), int(d['max_game_depth'])),
+            'action_selector1': d['action_selector1'],
+            'action_selector2': d['action_selector2'],
+            'avg.time_total': float(d['avg.time_total']) if 'avg.time_total' in d else None,
+            'avg.num_submodels': float(d['avg.num_submodels']) if 'avg.num_submodels' in d else None,
+            # 'sum.result_0': int(d['sum.result_0']),
+            'sum.result_1': int(d['sum.result_1']),
+            'sum.timeouts': int(d['sum.timeouts']),
+            'stddev.time_total': float(d['stddev.time_total']) if 'stddev.time_total' in d else None,
             }
 
 def process_first():
@@ -200,10 +225,15 @@ def process_second():
 
 import evoplotter.reporting
 from pathlib import Path
+import shutil
 
 
-def create_report():
-    results_dir = Path("results_final")
+def create_report(data, report_dir_path):
+    results_dir = report_dir_path
+    if results_dir.exists():
+        shutil.rmtree(results_dir)
+    os.mkdir(results_dir)
+
     # text3 = data3.style \
     #     .format(precision=6, thousands=" ", decimal=".", na_rep="--") \
     #     .background_gradient(subset=[('Time')], axis=None) \
@@ -214,51 +244,98 @@ def create_report():
 
     report = evoplotter.reporting.ReportPDF(packages=["multirow"],
                                             geometry_params="[paperwidth=35cm, paperheight=100cm, margin=0.3cm]")
+
+    s0 = evoplotter.reporting.SectionRelative("Full data")
+    s0.add(data["table_full"])
+
     s1_exp1 = evoplotter.reporting.SectionRelative("Experiment 1")
-    ss1 = evoplotter.reporting.SectionRelative("Original data")
-    # ss1.add(text)
-    ss2 = evoplotter.reporting.SectionRelative("Processed data", label="sec:exp1-processed-data")
+    ss1 = evoplotter.reporting.SectionRelative(r"action\_selector1=1-best (proponent chooses only 1 action)")
+    ss1.add(data["table_depthPercent_1"])
+    ss1.add(data["table_decision1_1"])
+    ss1.add(data["table_depthPercent_cases0_1"])
+    ss1.add(data["table_decision1_cases0_1"])
+
+    ss2 = evoplotter.reporting.SectionRelative(r"action\_selector1=all (proponent takes all actions)")
     ss2.add(r"\begin{minipage}{25cm}"
-            r"Aggregating runs of the experiment by pairs (Benchmark, IsReduced) and averaging processing times. "
-            r"IsReduced=True means that a random strategy was generated for all agents in the coalition, their local"
-            r" models were reduced/cut to reflect this, new file representing model was generated, and then finally processed by STV. "
             r"IsReduced=False means that a benchmark was processed as is by STV (a baseline we compare with)." + "\n"
-                                                                                                                 r"\end{minipage}\vspace{0.5cm}" + "\n\n" + r"\noindent")
-    # ss2.add(text2)
+            r"\end{minipage}\vspace{0.5cm}" + "\n\n" + r"\noindent")
+    ss2.add(data["table_depthPercent_all"])
+    ss2.add(data["table_decision1_all"])
+    ss2.add(data["table_depthPercent_cases0_all"])
+    ss2.add(data["table_decision1_cases0_all"])
 
     ss3 = evoplotter.reporting.SectionRelative("Processing time ratios")
     ss3.add(r"\begin{minipage}{25cm}"
             r"This table is computed based on the table in Section~\ref{sec:exp1-processed-data}. r/nr-ratio is computed "
-            r"as (time reduced) / (time not reduced). A r/nr-ratio equal to 1 means that reduction had no impact on time. "
-            r"We would like for the r/nr-ratio to be as small as possible, since that would imply that verifying a single "
-            r"strategy is more efficient than verifying the whole problem. "
             r"" + "\n" + r"\end{minipage}\vspace{0.5cm}" + "\n\n" + r"\noindent")
     # ss3.add(text3)
     # ss_errors = evoplotter.reporting.SectionRelative("Runs with STV errors (exit code != 0)")
     # ss_errors.add(text_errors)
-    # s1_exp1.add(ss1)  # a table with original data is too big; this data is accessible as a csv
+    s1_exp1.add(ss1)
     s1_exp1.add(ss2)
-    s1_exp1.add(ss3)
+    # s1_exp1.add(ss3)
+    report.add(s0)
     report.add(s1_exp1)
     f = results_dir / "report_final.tex"
     report.save_and_compile(f, output_dir=f.parent)
 
 
-def process_final(summary_folders):
+def get_latex_table_default(df):
+    return df.to_latex(float_format=lambda x: "{:.2f}".format(x))
+
+
+def get_latex_table_pivot1(df, values):
+    df = df.pivot_table(values=values, index=["n", "k"], columns=["depth_percent"], dropna=False)
+    # cmap="Greys",
+    text = df.style \
+        .format(precision=2, thousands=" ", decimal=".", na_rep="--") \
+        .background_gradient(axis=None) \
+        .applymap(lambda x: 'color: black; background-color: white' if pd.isnull(x) else '') \
+        .to_latex(convert_css=True, hrules=True)
+    text = text.replace("_", r"\_") + r"\\"  # .replace("0000", "")
+    return text
+
+
+def process_final(summary_folders, report_dir_path):
+    data = {}
     dicts = []
     for sf in summary_folders:
         for p in load_properties_from_dir(sf):
             dicts.append(process_dict(p))
 
-    data_opp = pd.DataFrame.from_records(dicts)
-    print(data_opp)
 
-    create_report()
+    df = pd.DataFrame.from_records(dicts)
+    df.sort_values(by=["n", "k", "max_game_depth", "action_selector1", "action_selector2"], inplace=True)
+    print(df.dtypes)
+    data["table_full"] = get_latex_table_default(df)
+    print(df)
+
+    df2_1 = df[(df["k"] == 3) & (df["action_selector1"] == "1-best")]
+    df2_2 = df[(df["k"] == 3) & (df["action_selector1"] == "all")]
+    df3_1 = df[(df["k"] != 3) & (df["action_selector1"] == "1-best")]
+    df3_2 = df[(df["k"] != 3) & (df["action_selector1"] == "all")]
+
+    data["table_depthPercent_1"] = get_latex_table_pivot1(df2_1, values=["avg.time_total"])
+    data["table_decision1_1"] = get_latex_table_pivot1(df2_1, values=["sum.result_1"])
+    data["table_depthPercent_cases0_1"] = get_latex_table_pivot1(df3_1, values=["avg.time_total"])
+    data["table_decision1_cases0_1"] = get_latex_table_pivot1(df3_1, values=["sum.result_1"])
+
+    data["table_depthPercent_all"] = get_latex_table_pivot1(df2_2, values=["avg.time_total"])
+    data["table_decision1_all"] = get_latex_table_pivot1(df2_2, values=["sum.result_1"])
+    data["table_depthPercent_cases0_all"] = get_latex_table_pivot1(df3_2, values=["avg.time_total"])
+    data["table_decision1_cases0_all"] = get_latex_table_pivot1(df3_2, values=["sum.result_1"])
+
+    create_report(data, report_dir_path=report_dir_path)
 
 
 
-summary_folders = ["results_final/summary", "results_final/summary_correct_timeout"]
-process_final(summary_folders)
+# summary_folders = ["exp_final/summary", "exp_final_correct_timeout/summary"]
+summary_folders = ["exp_final_tmp/summary_1", "exp_final_tmp/summary_2"]
+process_final(summary_folders, report_dir_path=Path("exp_final_report"))
+
+
+summary_folders = ["exp_final_r2s2000/summary"]
+process_final(summary_folders, report_dir_path=Path("exp_final_report_r2s2000"))
 
 # process_first()
 # process_second()
